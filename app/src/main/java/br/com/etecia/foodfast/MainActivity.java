50b3582118af2ff036375f5cfa30a7f6f02d7771
YPhill <phelipequeiroz2004@gmail.com>
2022-10-27 21:59:07 -0300

    Toasts

--- app/src/main/java/br/com/etecia/foodfast/MainActivity.java
+++ app/src/main/java/br/com/etecia/foodfast/MainActivity.java
@@ -1,10 +1,13 @@
 package br.com.etecia.foodfast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +21,53 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
+        inflater.inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+    //Criando o método de clicar nos itens do menu
 
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mFav:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei nos favoritos",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mBuscar:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no buscar",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mConfiguracao:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei nas configurações",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mHistorico:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no histórico",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mSobre:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no sobre",
+                        Toast.LENGTH_SHORT).show();
+                break;
 
 
+            case R.id.mCompartilhar:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei nos compartilhar",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
 
-
+        return super.onOptionsItemSelected(item);
+    }
 }
